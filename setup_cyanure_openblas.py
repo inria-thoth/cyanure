f044e58688d50bd00ae3ef5ba90cfd2d26276d1d
tomMoral <thomas.moreau.2010@gmail.com>
2020-03-17 18:40:14 +0100

    FIX dependency on scipy in setup

--- setup_cyanure_openblas.py
+++ setup_cyanure_openblas.py
@@ -1,23 +1,26 @@
 from setuptools import setup, Extension
 import numpy
 
-libs = ['openblas','gomp']
+libs = ['openblas', 'gomp']
 
-cyanure_wrap=Extension('cyanure_wrap', 
-        libraries = libs,
-        include_dirs=[numpy.get_include(), '/usr/local/lib/'],
-        language='c++',
-        extra_compile_args = ['-DNDEBUG', '-DINT_64BITS', '-DAXPBY', '-fPIC', '-fopenmp', '-std=c++11', '-Wno-unused-function', '-Wno-write-strings' , '-fmax-errors=5'],
-        sources=['cyanure_wrap_module.cpp'])
+
+cyanure_wrap = Extension(
+    'cyanure_wrap',
+    libraries=libs,
+    include_dirs=[numpy.get_include(), '/usr/local/lib/'],
+    language='c++',
+    extra_compile_args=[
+        '-DNDEBUG', '-DINT_64BITS', '-DAXPBY', '-fPIC', '-fopenmp',
+        '-std=c++11', '-Wno-unused-function', '-Wno-write-strings',
+        '-fmax-errors=5'],
+    sources=['cyanure_wrap_module.cpp'])
 
 setup(name='cyanure-openblas',
-        version='0.2',
-        author_email="julien.mairal@inria.fr",
-        license='bsd-3-clause',
-        url="http://julien.mairal.org/cyanure/",
-        description='optimization toolbox for machine learning',
-#        install_requires=['openblas'],
-        ext_modules=[cyanure_wrap],
-        py_modules=['cyanure'])
-
-
+      version='0.2',
+      author_email="julien.mairal@inria.fr",
+      license='bsd-3-clause',
+      url="http://julien.mairal.org/cyanure/",
+      description='optimization toolbox for machine learning',
+      install_requires=['scipy'],
+      ext_modules=[cyanure_wrap],
+      py_modules=['cyanure'])
--- setup_cyanure_openblas_no_openmp.py
+++ setup_cyanure_openblas_no_openmp.py
@@ -3,21 +3,24 @@ import numpy
 
 libs = ['openblas']
 
-cyanure_wrap=Extension('cyanure_wrap', 
-        libraries = libs,
-        include_dirs=[numpy.get_include()],
-        language='c++',
-        extra_compile_args = ['-DNDEBUG', '-DINT_64BITS', '-DAXPBY', '-fPIC', '-std=c++11', '-Wno-unused-function', '-Wno-write-strings' , '-fmax-errors=5', '-w'],
-        sources=['cyanure_wrap_module.cpp'])
+
+cyanure_wrap = Extension(
+    'cyanure_wrap',
+    libraries=libs,
+    include_dirs=[numpy.get_include()],
+    language='c++',
+    extra_compile_args=[
+            '-DNDEBUG', '-DINT_64BITS', '-DAXPBY', '-fPIC', '-std=c++11',
+            '-Wno-unused-function', '-Wno-write-strings', '-fmax-errors=5',
+            '-w'],
+    sources=['cyanure_wrap_module.cpp'])
 
 setup(name='cyanure-openblas-no-openmp',
-        version='0.2',
-        author_email="julien.mairal@inria.fr",
-        license='bsd-3-clause',
-        url="http://julien.mairal.org/cyanure/",
-        description='optimization toolbox for machine learning',
-#        install_requires=['openblas'],
-        ext_modules=[cyanure_wrap],
-        py_modules=['cyanure'])
-
-
+      version='0.2',
+      author_email="julien.mairal@inria.fr",
+      license='bsd-3-clause',
+      url="http://julien.mairal.org/cyanure/",
+      description='optimization toolbox for machine learning',
+      install_requires=['scipy'],
+      ext_modules=[cyanure_wrap],
+      py_modules=['cyanure'])
