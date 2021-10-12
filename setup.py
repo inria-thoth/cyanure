3394ada37b323765787d7bc1ed85778333f2d163
Thomas Ryckeboer <37658138+thomasryck@users.noreply.github.com>
2021-10-12 12:23:48 +0200

    chore(): other location

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ import os
 if platform.system() == "Darwin":
     os.environ["CC"] = "/usr/local/opt/llvm/bin/clang"
     os.environ["CXX"] = "/usr/local/opt/llvm/bin/clang++"
-    os.environ["DOpenMP_omp_LIBRARY"] = "/usr/local/lib/libomp.dylib"
-    os.environ["DOpenMP_libomp_LIBRARY"] = "/usr/local/lib/libomp.dylib"
+    os.environ["DOpenMP_omp_LIBRARY"] = "/usr/local/Cellar/libomp/12.0.1/lib/libomp.dylib"
+    os.environ["DOpenMP_libomp_LIBRARY"] = "/usr/local/Cellar/libomp/12.0.1/lib/libomp.dylib"
 
 
 def getBlas():
@@ -74,12 +74,12 @@ else:
         if platform.system() == "Darwin":            
             os.system("find /usr -xdev -name '*libomp*' 2>/dev/null")
             os.system("brew --prefix libomp")
-            os.system("export LIBRARY_PATH=$LIBRARY_PATH: /usr/local/Cellar/llvm/12.0.1/lib/")
-            os.system("export LD_LIBRARY_PATH=$LD_LIBRARY_PATH: /usr/local/Cellar/llvm/12.0.1/lib/")
-            os.system("export PATH= /usr/local/Cellar/llvm/12.0.1/lib/:$PATH")
+            os.system("export LIBRARY_PATH=$LIBRARY_PATH: /usr/local/Cellar/libomp/12.0.1/lib/")
+            os.system("export LD_LIBRARY_PATH=$LD_LIBRARY_PATH: /usr/local/Cellar/libomp/12.0.1/lib/")
+            os.system("export PATH= /usr/local/Cellar/libomp/12.0.1/lib/:$PATH")
 
             INCLUDE_DIRS = ['/usr/local/opt/llvm/include', '/usr/local/opt/openblas/include', '/usr/local/include', "/usr/local/opt/libomp/include"] + INCLUDE_DIRS
-            LIBRARY_DIRS = ['/usr/local/opt/openblas/lib', '/usr/local/lib', "/usr/local/opt/libomp/lib"]
+            LIBRARY_DIRS = ['/usr/local/opt/openblas/lib', '/usr/local/lib', "/usr/local/Cellar/libomp/12.0.1/lib/"]
             LIBS = LIBS + ['libomp']
             RUNTIME_LIRABRY_DIRS=LIBRARY_DIRS
 
