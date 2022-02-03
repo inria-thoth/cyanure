dfc0ecbfaa53378cd31ab8404e4b19c759ed0890
Thomas Ryckeboer <37658138+thomasryck@users.noreply.github.com>
2022-02-03 16:20:41 +0100

    chore: try

--- setup.py
+++ setup.py
@@ -22,8 +22,8 @@ class sdistzip(_sdist):
 
 if platform.system() == "Darwin":
     pass
-    # os.environ["CC"] = "/usr/bin/clang"
-    # os.environ["CXX"] = "/usr/bin/clang++"
+    os.environ["CC"] = "/usr/local/opt/llvm/bin/clang"
+    os.environ["CXX"] = "/usr/local/opt/llvm/bin/clang++"
     # os.environ["LDFLAGS"] = '-Wl,-rpath,/usr/local/opt/libomp/lib -L/usr/local/opt/libomp/lib -lomp'
     # os.environ["CPPFLAGS"] = '-Xpreprocessor -fopenmp'
 
