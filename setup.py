d5493fc3f478017d9bcb472cd9b214873b634258
Thomas Ryckeboer <37658138+thomasryck@users.noreply.github.com>
2022-02-03 16:49:40 +0100

    chore: search

--- setup.py
+++ setup.py
@@ -134,6 +134,7 @@ else:
         EXTRA_COMPILE_ARGS = EXTRA_COMPILE_ARGS
 
         if platform.system == "Darwin":
+            os.system("find /usr -xdev -name '*omp*' 2>/dev/null")
             INCLUDE_DIRS = ["/usr/local/include", "/usr/local/opt/llvm/include"] + INCLUDE_DIRS
             LIBRARY_DIRS = ["/usr/local/lib", "/usr/local/opt/llvm/lib"] + LIBRARY_DIRS
 
