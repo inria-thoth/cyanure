d98056b36ad59bb47f48b6ab81099f7b44debc40
Thomas Ryckeboer <37658138+thomasryck@users.noreply.github.com>
2022-03-11 13:09:45 +0100

    ci: install numpy

--- docs/sphinx/source/conf.py
+++ docs/sphinx/source/conf.py
@@ -19,6 +19,7 @@ import sys
 def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
+install("numpy")
 install("../../../")
 sys.path.insert(0, os.path.abspath('../../../'))
 
