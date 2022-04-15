cd3270785da40ade8c4a0dcd974808e59b000858
Thomas Ryckeboer <37658138+thomasryck@users.noreply.github.com>
2022-04-15 17:10:23 +0200

    ci: build doc on master

--- docs/sphinx/source/conf.py
+++ docs/sphinx/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Julien Mairal'
 author = 'Julien Mairal'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2'
+release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
