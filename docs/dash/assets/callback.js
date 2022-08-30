4cc9bc9a747597a8e5e24190d573f1a7d725aaa9
Thomas Ryckeboer <37658138+thomasryck@users.noreply.github.com>
2022-08-30 17:21:55 +0200

    docs: better frontend

--- docs/dash/assets/callback.js
+++ docs/dash/assets/callback.js
@@ -33,8 +33,10 @@ window.dash_clientside.clientside = {
 	var new_fig = {};
 	new_fig['data'] = [];
 	new_fig['layout'] = fig['layout'];
-	var max = 100;
+	var max = 0;
 	var max_data = 0;
+    var min = 1000000;
+	var min_data = 0;
     //new_fig['layout']['annotations'][0]['visible'] = false;
     //new_fig['layout']['annotations'][1]['visible'] = true;
     for (i = 0; i < fig['data'].length; i++) {
@@ -54,8 +56,12 @@ window.dash_clientside.clientside = {
         if (name.includes(dataset) && name.includes(core) && name.includes(implementation) && name.includes(solver)){
             new_fig['data'].push(fig['data'][i]);
             max_data = Math.max(...fig['data'][i]['y']);
+            min_data = Math.min(...fig['data'][i]['y']);
             if (max_data > max){
                 max = max_data;
+            }
+            if (min_data < min){
+                min = min_data;
             }
         }
     }
@@ -64,8 +70,11 @@ window.dash_clientside.clientside = {
 	if (log_or_lin === 'log'){
 	    new_fig['layout']['legend']['x'] = .65;
 	    new_fig['layout']['legend']['y'] = .1;
-	    new_fig['layout']['yaxis']['range'] = [1.2, Math.log10(max)];
-	    new_fig['layout']['yaxis']['autorange'] = false;
+        if(max < 1){
+            max = 1
+        }
+	    new_fig['layout']['yaxis']['range'] = [Math.log10(min) , Math.log10(max)];
+	    new_fig['layout']['yaxis']['autorange'] = true;
 	}
 	else{
 	    new_fig['layout']['legend']['x'] = .05;
