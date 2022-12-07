8a0c82d31e003d5a3006d45d75663c8a96d5009e
Thomas Ryckeboer <37658138+thomasryck@users.noreply.github.com>
2022-12-07 10:26:31 +0100

    debug: mac

--- test/test_logistic_regression.py
+++ test/test_logistic_regression.py
@@ -972,6 +972,8 @@ def test_elastic_net_versus_sgd(C, multiplier):
     log.fit(X, y)
     print(sgd.coef_.shape)
     print(np.squeeze(sgd.coef_).shape)
+    print(log.coef_.shape)
+    print(np.transpose(log.coef_).shape)
     assert_array_almost_equal(np.squeeze(sgd.coef_), np.transpose(log.coef_), decimal=1)
 
 @pytest.mark.parametrize(
