fc0d16bea721951ef48bcd3fdc2dd9e3cc990b4a
Thomas Ryckeboer <37658138+thomasryck@users.noreply.github.com>
2022-12-12 17:06:21 +0100

    debug

--- build_script/macos.sh
+++ build_script/macos.sh
@@ -11,16 +11,21 @@ if [[ "$RUNNER_OS" == "macOS" ]]; then
     # supported version of the macos SDK as libomp will be vendored into the
     # scikit-learn wheels for macos.
 
+    if [[ "$CIBW_BUILD" == *-macosx_arm64 ]]; then
+        # arm64 builds must cross compile because CI is on x64
+        export PYTHON_CROSSENV=1
+        # SciPy requires 12.0 on arm to prevent kernel panics
+        # https://github.com/scipy/scipy/issues/14688
+        # We use the same deployment target to match SciPy.
+        export MACOSX_DEPLOYMENT_TARGET=12.0
+        OPENMP_URL="https://anaconda.org/conda-forge/llvm-openmp/11.1.0/download/osx-arm64/llvm-openmp-11.1.0-hf3c4609_1.tar.bz2"
+        OPENBLAS_URL="https://anaconda.org/conda-forge/libopenblas/0.3.21/download/osx-arm64/libopenblas-0.3.21-openmp_hc731615_3.tar.bz2"
+    else
+        export MACOSX_DEPLOYMENT_TARGET=10.9
+        OPENMP_URL="https://anaconda.org/conda-forge/llvm-openmp/11.1.0/download/osx-64/llvm-openmp-11.1.0-hda6cdc1_1.tar.bz2"
+        OPENBLAS_URL="https://anaconda.org/conda-forge/libopenblas/0.3.21/download/osx-64/libopenblas-0.3.21-openmp_h429af6e_3.tar.bz2"
+    fi
 
-    # arm64 builds must cross compile because CI is on x64
-    export PYTHON_CROSSENV=1
-    # SciPy requires 12.0 on arm to prevent kernel panics
-    # https://github.com/scipy/scipy/issues/14688
-    # We use the same deployment target to match SciPy.
-    export MACOSX_DEPLOYMENT_TARGET=12.0
-    OPENMP_URL="https://anaconda.org/conda-forge/llvm-openmp/11.1.0/download/osx-arm64/llvm-openmp-11.1.0-hf3c4609_1.tar.bz2"
-    OPENBLAS_URL="https://anaconda.org/conda-forge/libopenblas/0.3.21/download/osx-arm64/libopenblas-0.3.21-openmp_hc731615_3.tar.bz2"
-  
     sudo conda create -n build_openmp $OPENMP_URL
     sudo conda create -n build_blas $OPENBLAS_URL
 
