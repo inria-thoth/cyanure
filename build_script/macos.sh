9071ad667a5d18d55606936ebdf58f3a4a8b339c
Thomas Ryckeboer <37658138+thomasryck@users.noreply.github.com>
2022-12-12 17:33:22 +0100

    debug: unwind

--- build_script/macos.sh
+++ build_script/macos.sh
@@ -12,6 +12,11 @@ if [[ "$RUNNER_OS" == "macOS" ]]; then
     # scikit-learn wheels for macos.
 
     if [[ "$CIBW_BUILD" == *-macosx_arm64 ]]; then
+        export MACOSX_DEPLOYMENT_TARGET=10.9
+        OPENMP_URL="https://anaconda.org/conda-forge/llvm-openmp/11.1.0/download/osx-64/llvm-openmp-11.1.0-hda6cdc1_1.tar.bz2"
+        OPENBLAS_URL="https://anaconda.org/conda-forge/libopenblas/0.3.21/download/osx-64/libopenblas-0.3.21-openmp_h429af6e_3.tar.bz2"
+        UNWIND_URL="https://anaconda.org/conda-forge/libosxunwind/0.0.6/download/osx-64/libosxunwind-0.0.6-h940c156_0.tar.bz2"
+    else
         # arm64 builds must cross compile because CI is on x64
         export PYTHON_CROSSENV=1
         # SciPy requires 12.0 on arm to prevent kernel panics
@@ -20,16 +25,14 @@ if [[ "$RUNNER_OS" == "macOS" ]]; then
         export MACOSX_DEPLOYMENT_TARGET=12.0
         OPENMP_URL="https://anaconda.org/conda-forge/llvm-openmp/11.1.0/download/osx-arm64/llvm-openmp-11.1.0-hf3c4609_1.tar.bz2"
         OPENBLAS_URL="https://anaconda.org/conda-forge/libopenblas/0.3.21/download/osx-arm64/libopenblas-0.3.21-openmp_hc731615_3.tar.bz2"
-    else
-        export MACOSX_DEPLOYMENT_TARGET=10.9
-        OPENMP_URL="https://anaconda.org/conda-forge/llvm-openmp/11.1.0/download/osx-64/llvm-openmp-11.1.0-hda6cdc1_1.tar.bz2"
-        OPENBLAS_URL="https://anaconda.org/conda-forge/libopenblas/0.3.21/download/osx-64/libopenblas-0.3.21-openmp_h429af6e_3.tar.bz2"
+        UNWIND_URL="https://anaconda.org/conda-forge/libosxunwind/0.0.6/download/osx-arm64/libosxunwind-0.0.6-hc021e02_0.tar.bz2"
     fi
 
     sudo conda create -n build_openmp $OPENMP_URL
     sudo conda create -n build_blas $OPENBLAS_URL
+    sudo conda create -n build_unwind $UNWIND_URL
 
     sudo cp "/usr/local/miniconda/envs/build_blas/lib/libopenblas.0.dylib" "/usr/local/miniconda/envs/build_blas/lib/libopenblas.dylib"
 
-    ls "/usr/local/miniconda/envs/build_blas/lib"
+    ls "/usr/local/miniconda/envs/build_unwind/lib"
 fi
