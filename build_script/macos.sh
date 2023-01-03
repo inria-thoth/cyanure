e4b327620b1808b15fc3e63705f5a9cd4f87dc95
Thomas Ryckeboer <37658138+thomasryck@users.noreply.github.com>
2023-01-03 15:12:43 +0100

    debug: test build

--- build_script/macos.sh
+++ build_script/macos.sh
@@ -3,20 +3,13 @@
 set -e
 set -x
 
-printenv
-
 # OpenMP is not present on macOS by default
 if [[ "$RUNNER_OS" == "macOS" ]]; then
     # Make sure to use a libomp version binary compatible with the oldest
     # supported version of the macos SDK as libomp will be vendored into the
     # scikit-learn wheels for macos.
 
-    if [[ "$CIBW_BUILD" == *-macosx_arm64 ]]; then
-        export MACOSX_DEPLOYMENT_TARGET=10.9
-        OPENMP_URL="https://anaconda.org/conda-forge/llvm-openmp/11.1.0/download/osx-64/llvm-openmp-11.1.0-hda6cdc1_1.tar.bz2"
-        OPENBLAS_URL="https://anaconda.org/conda-forge/libopenblas/0.3.21/download/osx-64/libopenblas-0.3.21-openmp_h429af6e_3.tar.bz2"
-        GFORTRAN_URL="https://anaconda.org/conda-forge/libgfortran5/11.3.0/download/osx-64/libgfortran5-11.3.0-h082f757_26.tar.bz2"
-    else
+    if [[ "$ARCHFLAGS" == *-arch arm64 ]]; then
         # arm64 builds must cross compile because CI is on x64
         export PYTHON_CROSSENV=1
         # SciPy requires 12.0 on arm to prevent kernel panics
@@ -27,6 +20,12 @@ if [[ "$RUNNER_OS" == "macOS" ]]; then
         OPENMP_URL="https://anaconda.org/conda-forge/llvm-openmp/11.1.0/download/osx-arm64/llvm-openmp-11.1.0-hf3c4609_1.tar.bz2"
         OPENBLAS_URL="https://anaconda.org/conda-forge/libopenblas/0.3.21/download/osx-arm64/libopenblas-0.3.21-openmp_hc731615_3.tar.bz2"
         GFORTRAN_URL="https://anaconda.org/conda-forge/libgfortran5/11.3.0/download/osx-arm64/libgfortran5-11.3.0-hdaf2cc0_26.tar.bz2"
+        
+    else
+        export MACOSX_DEPLOYMENT_TARGET=10.9
+        OPENMP_URL="https://anaconda.org/conda-forge/llvm-openmp/11.1.0/download/osx-64/llvm-openmp-11.1.0-hda6cdc1_1.tar.bz2"
+        OPENBLAS_URL="https://anaconda.org/conda-forge/libopenblas/0.3.21/download/osx-64/libopenblas-0.3.21-openmp_h429af6e_3.tar.bz2"
+        GFORTRAN_URL="https://anaconda.org/conda-forge/libgfortran5/11.3.0/download/osx-64/libgfortran5-11.3.0-h082f757_26.tar.bz2"
     fi
 
     sudo conda create -n build_openmp $OPENMP_URL
@@ -34,6 +33,4 @@ if [[ "$RUNNER_OS" == "macOS" ]]; then
     sudo conda create -n build_gfortran $GFORTRAN_URL
 
     sudo cp "/usr/local/miniconda/envs/build_blas/lib/libopenblas.0.dylib" "/usr/local/miniconda/envs/build_blas/lib/libopenblas.dylib"
-
-    sudo ls /usr/local/miniconda/envs/build_openmp/lib/
 fi
