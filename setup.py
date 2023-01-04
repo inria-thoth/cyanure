57c696d1aad668965b16d45cf65e03809a46dcfc
Thomas Ryckeboer <37658138+thomasryck@users.noreply.github.com>
2023-01-04 15:58:47 +0100

    debug

--- setup.py
+++ setup.py
@@ -103,7 +103,7 @@ else:
 
         if platform.system() == "Darwin":
             print(numpy.get_include())
-            INCLUDE_DIRS = ["/usr/local/miniconda/envs/build_blas/include", "/usr/local/miniconda/envs/build_openmp/include", "/usr/local/miniconda/envs/build_gfortran/include"] + [numpy.get_include()]
+            INCLUDE_DIRS = ["/usr/local/miniconda/envs/build_blas/include", "/usr/local/miniconda/envs/build_openmp/include", "/usr/local/miniconda/envs/build_gfortran/include"] 
             LIBRARY_DIRS = ["/usr/local/miniconda/envs/build_openmp/lib", "/usr/local/miniconda/envs/build_blas/lib", "/usr/local/miniconda/envs/build_gfortran/lib"] 
             EXTRA_COMPILE_ARGS = [
             '-DINT_64BITS', '-DAXPBY', '-fPIC',
