4074a1f66e27565acde868dc34a124c861bfdd90
Thomas Ryckeboer <37658138+thomasryck@users.noreply.github.com>
2023-01-05 14:50:07 +0100

    debug

--- build_script/macos.sh
+++ build_script/macos.sh
@@ -38,7 +38,7 @@ if [[ "$RUNNER_OS" == "macOS" ]]; then
     PREFIX="$CONDA_HOME/envs/build"
 
     export CC=/usr/bin/clang
-    export CXX=/usr/bin/clang++
+    export CXX=/usr/local/opt/llvm/bin/clang++
     export CPPFLAGS="$CPPFLAGS -Xpreprocessor -fopenmp"
     export CFLAGS="$CFLAGS -I$PREFIX/include"
     export CXXFLAGS="$CXXFLAGS -I$PREFIX/include"
