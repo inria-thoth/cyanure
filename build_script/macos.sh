068c42b7be55af137b70c84936042735c034d7a5
Thomas Ryckeboer <37658138+thomasryck@users.noreply.github.com>
2023-01-05 10:40:14 +0100

    debug

--- build_script/macos.sh
+++ build_script/macos.sh
@@ -34,7 +34,7 @@ if [[ "$RUNNER_OS" == "macOS" ]]; then
 
     sudo cp "/usr/local/miniconda/envs/build_blas/lib/libopenblas.0.dylib" "/usr/local/miniconda/envs/build_blas/lib/libopenblas.dylib"
     
-    sudo ls /usr/local/miniconda/envs/build_openmp/lib
+    sudo ls /usr/local/miniconda/envs/build_openmp/lib/clang
 
     sudo otool -L /usr/local/miniconda/envs/build_openmp/lib/libomp.dylib
 fi
