296b21f5a9a36d60937c61ed2086a2feec6e5db3
Thomas Ryckeboer <37658138+thomasryck@users.noreply.github.com>
2023-01-19 15:54:41 +0100

    refacto: move benchmark files to a private repository

--- benchmark/activate_environment.sh
+++
@@ -1,1 +0,0 @@
-/home/tryckeboer/benchmark/launch_experiments.sh
--- benchmark/oar_jobs.sh
+++
@@ -1,4 +0,0 @@
-for CPU_CORES in 1 2 4 8 16 32
-do
-	oarsub -t exotic -p yeti -l core=$CPU_CORES,walltime=3 /home/tryckeboer/benchmark/activate_environment.sh
-done
