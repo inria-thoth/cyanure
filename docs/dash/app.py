b9848d1e6c97363fbe588ec2a2d2621b0f1aefea
Thomas Ryckeboer <37658138+thomasryck@users.noreply.github.com>
2023-03-01 16:22:28 +0100

    ci: debug  build

--- docs/dash/app.py
+++ docs/dash/app.py
@@ -53,7 +53,7 @@ def make_plot(list_of_csv):
         df_temporary = pd.read_csv(path)
         print(path)
         print(df_temporary.head())
-        print(df_temporary["timestamp"])
+        #print(df_temporary["timestamp"])
         quit()
         fig.add_trace(go.Scatter(x=df_temporary["timestamp"] / 1000,
                                  y=df_temporary["Relative optimality gap"], mode='lines',
