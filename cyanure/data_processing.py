e3d689dc06df19901dc97298e6912bb84dcfd832
RYCKEBOER Thomas <37658138+thomasryck@users.noreply.github.com>
2025-03-18 17:16:36 +0100

    warning

--- cyanure/data_processing.py
+++ cyanure/data_processing.py
@@ -190,7 +190,7 @@ def type_of_target(y, input_name=""):
     check_y_kwargs = dict(
         accept_sparse=True,
         allow_nd=True,
-        force_all_finite=False,
+        ensure_all_finite=False,
         ensure_2d=False,
         ensure_min_samples=0,
         ensure_min_features=0,
@@ -257,7 +257,7 @@ def is_multilabel(y):
         check_y_kwargs = dict(
             accept_sparse=True,
             allow_nd=True,
-            force_all_finite=False,
+            ensure_all_finite=False,
             ensure_2d=False,
             ensure_min_samples=0,
             ensure_min_features=0,
