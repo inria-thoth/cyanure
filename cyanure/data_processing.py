f5d4e1eb493ed38f8239c8a4327849f1e2e39463
RYCKEBOER Thomas <37658138+thomasryck@users.noreply.github.com>
2025-03-19 11:55:24 +0100

    debug

--- cyanure/data_processing.py
+++ cyanure/data_processing.py
@@ -591,7 +591,7 @@ def check_input_fit(X, labels, estimator):
     if X.shape[0] == 1:
         raise ValueError("There should have more than 1 sample")
 
-    if not estimator.target_tags.multi_output and labels.ndim > 1:
+    if not estimator.__sklearn_tags__().multi_output and labels.ndim > 1:
         warnings.warn(
             "A column-vector y was passed when a 1d array was expected", DataConversionWarning)
 
