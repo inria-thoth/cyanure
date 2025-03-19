e24250e9c8c7231ba6b32bdfee89010da60fbeba
RYCKEBOER Thomas <37658138+thomasryck@users.noreply.github.com>
2025-03-19 17:27:12 +0100

    debug

--- cyanure/estimators.py
+++ cyanure/estimators.py
@@ -336,6 +336,7 @@ class ERM(BaseEstimator, ABC):
                 self.optimization_info_, nclasses, axis=0)
 
         print(self.optimization_info_)
+        print(self.optimization_info_.shape)
 
         self.n_iter_ = np.array([self.optimization_info_[class_index][0][-1]
                                 for class_index in range(self.optimization_info_.shape[0])])
