ab8c2d28191a6feff4d88ad802d6555d4e1dc1a4
RYCKEBOER Thomas <37658138+thomasryck@users.noreply.github.com>
2025-03-20 14:44:10 +0100

    debug

--- cyanure_lib/lib/convert_cxx_to_python.h
+++ cyanure_lib/lib/convert_cxx_to_python.h
@@ -180,7 +180,7 @@ static void optimInfoToNpy(PyArrayObject *array, OptimInfo<T> &matrix, std::stri
     for (npy_intp i = 0; i < nclass; i++) {
         for (npy_intp j = 0; j < m; j++) {
             for (npy_intp k = 0; k < n; k++) {
-                array_data[i * m * n + j * n + k] = matrix(i, j, k);
+                matrix(i, j, k);
             }
         }
     }
