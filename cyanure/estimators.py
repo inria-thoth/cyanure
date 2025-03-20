21ff48f523d901e10d1c706e465cb61469b43b5e
RYCKEBOER Thomas <37658138+thomasryck@users.noreply.github.com>
2025-03-20 10:50:17 +0100

    linting

--- cyanure/estimators.py
+++ cyanure/estimators.py
@@ -338,8 +338,6 @@ class ERM(BaseEstimator, ABC):
             self.optimization_info_ = np.repeat(
                 self.optimization_info_, nclasses, axis=0)
 
-        
-
         self.n_iter_ = np.array([self.optimization_info_[class_index][0][-1]
                                 for class_index in range(self.optimization_info_.shape[0])])
 
