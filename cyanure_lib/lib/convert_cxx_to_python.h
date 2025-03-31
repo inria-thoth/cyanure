96bb128532336d61a4b6a6f5dd963d9c2450fc53
RYCKEBOER Thomas <37658138+thomasryck@users.noreply.github.com>
2025-03-31 15:03:45 +0200

    debug: set zeros

--- cyanure_lib/lib/convert_cxx_to_python.h
+++ cyanure_lib/lib/convert_cxx_to_python.h
@@ -231,7 +231,10 @@ inline PyArrayObject *create_np_optim_info(const int nclass, const int m, const 
 {
     const int nd = 3;
     npy_intp dims[nd] = {nclass, m, n};
-    return (PyArrayObject *)PyArray_ZEROS(nd, dims, NPY_DOUBLE, 0);
+    PyObject *array_obj = (PyArrayObject *)PyArray_SimpleNewF(nd, dims, getTypeNumber<T>())
+    memset(PyArray_DATA((PyArrayObject *)array_obj), 0, PyArray_NBYTES((PyArrayObject *)array_obj));
+    return array_obj;
+
 }
 
 template <typename T>
